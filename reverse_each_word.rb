c71f27463ce698e1bad7077e467e935a0c8eec4c
Kolton Starr <starrkolton@gmail.com>
2019-04-30 18:45:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   funky_array = sentence.split(" ")
   reversed_array = []
-  funky_array.each do |word|
+  funky_array.collect do |word|
     reversed_array << word.reverse 
   end 
   reversed_array.join(" ")
