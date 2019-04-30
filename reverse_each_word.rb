62d9d369842c6218182d282a16ca6f69571ee985
Kolton Starr <starrkolton@gmail.com>
2019-04-30 18:44:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,9 @@
-#def reverse_each_word(sentence)
- # funky_array = sentence.split(" ")
-  #reversed_array = []
-  #funky_array.each do |word|
-   # reversed_array << word.reverse 
-  #end 
-  #reversed_array.join(" ")
-#end 
-
 def reverse_each_word(sentence)
   funky_array = sentence.split(" ")
-  funky_array.collect do |word|
-    arr = word.reverse
+  reversed_array = []
+  funky_array.each do |word|
+    reversed_array << word.reverse 
   end 
+  reversed_array.join(" ")
 end 
+
